515374198143f3f423b6ba145955c96d7bd59c34
AlvarezKnothValentin <finalhypno@hotmail.com>
2022-11-09 23:31:40 -0300

    error mercadopago

--- my-app/src/components/MetodosPago/MetodosPago.jsx
+++ my-app/src/components/MetodosPago/MetodosPago.jsx
@@ -4,42 +4,42 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import "./MetodosPago.css";
 import { Link } from 'react-router-dom';
-import mercadopago from 'mercadopago';
+// import mercadopago from 'mercadopago';
 
 const MetodosPagoC = () => {
   const [Select, setSelect] = useState("metodospago")
- 
-  const functMercado = () => {
-    fetch('http://localhost:3000/generar', (req, res) => {
 
-      let preference = {
-        back_urls: {
-          success: 'http://localhost:3003/success'
-        },
-        items: [
+  // const functMercado = () => {
+  //   fetch('http://localhost:3000/generar', (req, res) => {
 
-          {
-            id: 123,
-            title: 'Cabana 1 - 1 Persona',
-            unit_price: 4000,
-            currency_id: "ARS",
-            quantity: 1,
-          }
-        ],
-      };
+  //     let preference = {
+  //       back_urls: {
+  //         success: 'http://localhost:3003/success'
+  //       },
+  //       items: [
 
-      mercadopago.preferences
-        .create(preference)
-        .then(function (response) {
-          console.log(response.body.init_point);
-          res.send(`<a href="${response.body.init_point}">IR A PAGAR</a>`);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    });
-  }
-  
+  //         {
+  //           id: 123,
+  //           title: 'Cabana 1 - 1 Persona',
+  //           unit_price: 4000,
+  //           currency_id: "ARS",
+  //           quantity: 1,
+  //         }
+  //       ],
+  //     };
+
+  //     mercadopago.preferences
+  //       .create(preference)
+  //       .then(function (response) {
+  //         console.log(response.body.init_point);
+  //         res.send(`<a href="${response.body.init_point}">IR A PAGAR</a>`);
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error);
+  //       });
+  //   });
+  // }
+
   return (
     <Container fluid className='estilo'>
       <Form className='select'>
